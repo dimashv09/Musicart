ace5442ab831c48d25739653702d38c1e84c38a6
pindyakhabiibah <pindyakhabiibah@student.uns.ac.id>
2017-05-11 23:40:40 +0700

    admin site

---
+++ BMP/admin_site/admin/menu.php
@@ -0,0 +1,83 @@
+<aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- Sidebar user panel -->
+          <!-- search form -->
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="treeview">
+              <a href="#">
+                <i class="glyphicon glyphicon-file"></i> <span>News</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.php?page=berita"><i class="glyphicon glyphicon-pencil"></i> Input Artikel</a></li>
+                <li><a href="index.php?page=list_berita"><i class="glyphicon glyphicon-list active"></i> List Artikel</a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="glyphicon glyphicon-file"></i><span>Playlist</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.php?page=agenda"><i class="glyphicon glyphicon-pencil"></i> Input Playlist </a></li>
+                <li><a href="index.php?page=list_agenda"><i class="glyphicon glyphicon-list"></i> Playlist</a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="glyphicon glyphicon-file"></i><span>Video</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.php?page=agenda"><i class="glyphicon glyphicon-pencil"></i> Input Video </a></li>
+                <li><a href="index.php?page=list_agenda"><i class="glyphicon glyphicon-list"></i> List Video </a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="glyphicon glyphicon-file"></i><span>Agenda</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.php?page=agenda"><i class="glyphicon glyphicon-pencil"></i> Input Agenda</a></li>
+                <li><a href="index.php?page=list_agenda"><i class="glyphicon glyphicon-list"></i> List Agenda</a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="glyphicon glyphicon-file"></i><span>Gallery</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.php?page=agenda"><i class="glyphicon glyphicon-pencil"></i> Input Gambar</a></li>
+                <li><a href="index.php?page=list_agenda"><i class="glyphicon glyphicon-list"></i> List Gambar</a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="glyphicon glyphicon-file"></i><span>About Me</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.php?page=agenda"><i class="glyphicon glyphicon-pencil"></i> Edit AboutMe</a></li>
+                <li><a href="index.php?page=list_agenda"><i class="glyphicon glyphicon-list"></i> Lihat AboutMe</a></li>
+              </ul>
+            </li>
+            <li class="treeview">
+              <a href="#">
+                <i class="glyphicon glyphicon-file"></i><span>Contact</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="index.php?page=agenda"><i class="glyphicon glyphicon-pencil"></i> Input Contact</a></li>
+                <li><a href="index.php?page=list_agenda"><i class="glyphicon glyphicon-list"></i> List Contact</a></li>
+              </ul>
+            </li>
+            <li><a href="logout.php"><i class="glyphicon glyphicon-log-out"></i> <span>Logout</span></a></li>
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+</aside>
